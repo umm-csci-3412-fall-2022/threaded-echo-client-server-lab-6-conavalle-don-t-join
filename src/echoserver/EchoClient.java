43cd508b3c31b4ff7f3db990c7b1f3ec24f471cc
Conner <hetti031@morris.umn.edu>
2022-11-01 16:03:17 -0500

    We went through and created to working threads in client
    one is reader where it reads the output of the server and the other is writer which sends what you write to the server

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -6,7 +6,7 @@ import java.io.OutputStream;
 import java.net.Socket;
 
 public class EchoClient {
-	public static final int PORT_NUMBER = 6013;
+	public static final int portNumber = 6013;
 
 	public static void main(String[] args) throws IOException {
 		EchoClient client = new EchoClient();
@@ -14,10 +14,71 @@ public class EchoClient {
 	}
 
 	private void start() throws IOException {
-		Socket socket = new Socket("localhost", PORT_NUMBER);
-		InputStream socketInputStream = socket.getInputStream();
-		OutputStream socketOutputStream = socket.getOutputStream();
+	try{
+	 Socket socket = new Socket("127.0.0.1", portNumber);
+         OutputStream os = socket.getOutputStream();
+         InputStream is = socket.getInputStream();
 
-		// Put your code here.
+	 Thread coolNewAndFreshThread = new Thread(new reader(is));
+         coolNewAndFreshThread.start();
+ 	 Thread coolerNewerAnderFresherThreader = new Thread(new writer(os));
+	 coolerNewerAnderFresherThreader.start(); 
+         coolerNewerAnderFresherThreader.join();
+	 coolNewAndFreshThread.join();
+      // flush out the last bit of info beofre we shutdown
+      os.flush();
+      System.out.flush();
+      // We shutdown the connection instead of closing the socket
+      socket.shutdownOutput();
+      socket.shutdownInput();
+	} catch (Exception e) {
+	    System.err.println("Exception:  " + e);
+    }
 	}
+
+
+public class reader implements Runnable	{
+	InputStream is;
+
+	reader(InputStream input){
+		is = input;
+   }
+
+	@Override
+	public void run(){
+	try{
+	   int character;
+           while((character = is.read()) != -1){
+               System.out.write(character);
+        }
+	System.out.flush();
+	} catch (Exception e) {
+	     System.err.println("The Exception thing");
+	}
+
+   }
+
+
+}
+public class writer implements Runnable{
+     OutputStream os;
+
+     writer(OutputStream output){
+             os = output;
+     }
+
+     	@Override
+	public void run(){
+	try{
+		int c;
+		while ((c = System.in.read()) != -1) {
+             		 os.write((byte)c);
+             		 os.flush();
+	
+		}
+	} catch (Exception e) {
+            System.err.println("Exception:  " + e);
+	}
+     }
+}
 }
--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -9,23 +9,40 @@ import java.net.Socket;
 public class EchoServer {
 	
 	// REPLACE WITH PORT PROVIDED BY THE INSTRUCTOR
-	public static final int PORT_NUMBER = 0; 
+	public static final int PORT_NUMBER = 6013; 
 	public static void main(String[] args) throws IOException, InterruptedException {
 		EchoServer server = new EchoServer();
 		server.start();
 	}
 
 	private void start() throws IOException, InterruptedException {
-		ServerSocket serverSocket = new ServerSocket(PORT_NUMBER);
+	try{
+		ServerSocket sock = new ServerSocket(PORT_NUMBER);
 		while (true) {
-			Socket socket = serverSocket.accept();
+			Socket client = sock.accept();
+        		System.out.println("Got a request!");
+        		OutputStream os = client.getOutputStream();
+        		InputStream is = client.getInputStream();
+			int c;
+      // echos what the client sends back to the client
+      while ((c = is.read()) != -1) {
+          os.write((byte)c);
+          os.flush();
+      }
+      //Disconnect from the client
+      client.close();
+      System.out.println("Client Disconnected");
+
+	}
+    } catch (Exception e) {
+	    System.err.println("Exception:  " + e);
+      }
 
 			// Put your code here.
 			// This should do very little, essentially:
 			//   * Construct an instance of your runnable class
 			//   * Construct a Thread with your runnable
 			//      * Or use a thread pool
-			//   * Start that thread
-		}
-	}
+	}		//   * Start that thread
+	
 }
